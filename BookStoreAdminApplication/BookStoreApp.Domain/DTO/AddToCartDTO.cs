171dca07b7f7566f33f90c810d7cc3b221d88b19
Slobodanka Pishtolova <119663651+spishtolova@users.noreply.github.com>
2024-11-03 20:52:44 +0100

    Add files via upload

---
+++ BookStoreAdminApplication/BookStoreApp.Domain/DTO/AddToCartDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreApp.Domain.DTO
+{
+    public class AddToCartDTO
+    {
+        public Guid selectedBookId { get; set; }
+        public string? selectedBookTitle { get; set; }
+        public int quantity { get; set; }
+    }
+}
