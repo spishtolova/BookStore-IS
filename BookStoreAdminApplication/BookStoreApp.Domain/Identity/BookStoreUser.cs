6700844e5a8a63066e1ebcd7434921703e10f9c8
Slobodanka Pishtolova <119663651+spishtolova@users.noreply.github.com>
2024-11-03 20:53:29 +0100

    Add files via upload

---
+++ BookStoreAdminApplication/BookStoreApp.Domain/Identity/BookStoreUser.cs
@@ -0,0 +1,18 @@
+﻿using BookStoreApp.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreApp.Domain.Identity
+{
+    public class BookStoreUser : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Address { get; set; }
+        public ShoppingCart? UserCart { get; set; }
+    }
+}
