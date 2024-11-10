1569441110c8017f4456957ec0efd22503ad2157
Slobodanka Pishtolova <119663651+spishtolova@users.noreply.github.com>
2024-11-10 13:20:05 +0100

    Delete BookStoreAdminApplication directory

--- BookStoreAdminApplication/BookStoreApp.Domain/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BookStoreApp.Domain
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
